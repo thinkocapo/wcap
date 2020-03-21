fc76c648dabc583f5d414c922b6f715c808c8c82
William Capozzoli <thinkocapo@gmail.com>
2020-03-21 12:55:28 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: `WCAP`,
+    description: `Description of Travel & Photos, Books, Code Snippets, Accomplishments, Interests, Analysis`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+        resolve: `gatsby-theme-blog`,
+        options: {
+          // basePath defaults to `/`
+          basePath: `/blog`,
+        },
+      }
+  ]
+}
